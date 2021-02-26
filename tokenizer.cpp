06a2317637255e245001baeaa7b00e72dadc5c3b
Akantonio <adrianvantonio@yahoo.com>
2021-02-25 20:26:53 -0900

    declared functions in tokenizer

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -2,3 +2,8 @@
 // Created by Adrian Antonio on 2/23/2021.
 //
 
+#include "tokenizer.h"
+
+void printTokens(std::ostream &os,const vector<TokenAndPosition>&tokens){
+
+}
--- tokenizer.h
+++ tokenizer.h
@@ -5,4 +5,21 @@
 #ifndef HOMEWORK3_TOKENIZER_H
 #define HOMEWORK3_TOKENIZER_H
 
+#include<iostream>
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
+
+
+struct TokenAndPosition{
+    string _token;
+    int _line;
+    unsigned int _column;
+};
+
+vector<string> lineToTokens(const string &line);
+vector<TokenAndPosition> readLines();
+void printTokens(std::ostream &os,const vector<TokenAndPosition>&tokens);
+
 #endif //HOMEWORK3_TOKENIZER_H
