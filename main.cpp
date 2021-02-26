9bd2f1e631a0ae931d5d2c7ac79ebbfee8692613
Akantonio <adrianvantonio@yahoo.com>
2021-02-26 03:13:29 -0900

    Added catch header for better testing

--- main.cpp
+++ main.cpp
@@ -1,20 +1,3 @@
-#include "MoneyClass.h"
-#include <iostream>
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
 
-int main(int argc,char * argv[]) {
-    MoneyClass test(34.568);
-    MoneyClass test2;
-    MoneyClass test3(10,10);
-    MoneyClass test4(-444);
-    MoneyClass test5= test + test4;
-    MoneyClass test6(404);
-    test+=test3;
-    std::cout << test<<std::endl;
-    std::cout << test2<<std::endl;
-    std::cout << test3<<std::endl;
-    std::cout << test4<<std::endl;
-    std::cout << test5<<std::endl;
-    std::cout << test6<<std::endl;
-
-    return 0;
-}
---
+++ test.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Adrian Antonio on 2/26/2021.
+//
+
+#include "catch.hpp"
+#include "MoneyClass.h"
+
+
+TEST_CASE("MoneyClass"){
+    
+}
