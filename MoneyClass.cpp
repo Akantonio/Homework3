2ea64d8982f78883c1f2e65ff7a0092460aae1e8
Akantonio <adrianvantonio@yahoo.com>
2021-02-26 06:56:18 -0900

    fixed syntax error.

--- MoneyClass.cpp
+++ MoneyClass.cpp
@@ -29,7 +29,7 @@ std::ostream &operator<<(std::ostream & os, const Money &right) {
     int cents= right._value%100;
     if(right._value<0){
         dollars*= -1;
-        cents
+        cents*=-1;
         os<<"-$";
     }else{
         os<<"$";
